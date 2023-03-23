23db6e334994ba63d33c977859f6b343097b06ff
Omar Fares <omar.fsm02@gmail.com>
2023-03-23 00:13:47 -0400

    minor changes

--- Back-end/app.py
+++ Back-end/app.py
@@ -1,8 +1,11 @@
+import os
 import openai
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
 
-openai.api_key = "SOQXm0U1ajxq3E0ONzSiT3BlbkFJM2FGmbRX749VvDIn1Jvm"
+load_dotenv()  # load environment variables from .env file
+openai.api_key = os.getenv("OPENAI_API_KEY")
 app = Flask(__name__)
 CORS(app)  # add this line to enable CORS for all routes
 
@@ -10,27 +13,28 @@ CORS(app)  # add this line to enable CORS for all routes
 def predict():
     try:
         prompt = request.json['prompt']
-        message = generate_text(prompt)
+        print("message is as follow")
+        print("this is the user sent: "+prompt)
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt="I'm gonna be giving you a scenario and I want you to always start your first response (IMPORTANT: first only) with (sheeeesh, looks like we are predicting the future) or (Holy crap let's see what we got here, Hmmmm) or somthing funny like that ( you choose between 1, 2 or you creating one by yourself, so basically make your choice random please). Moreover, include some funny jokes and have a high schooler tone .IMPORTANT NOTE: if im not giving you a scenario that can be prictable (like for example saying hello), then just simply reply (ha? I'm here to pridct something, I don't have the time for a chat ( busy with some other users, HeHe (your the only one ^^))) Here is my scenario that i want you to predict what would happpen next:  "+prompt,
+            temperature=0.7,
+            max_tokens=3520,
+            top_p=1,
+            frequency_penalty=1,
+            presence_penalty=1,
+            api_key=openai.api_key,
+            n=1,  # set the number of responses to 1
+            stop=None,  # set the stopping sequence to None to generate only one response
+        )
+        message = response.choices[0].text.strip()
+        print(message)
         return jsonify({'message': message})
     except Exception as e:
         return jsonify({'error': str(e)})
 
 
-@app.route('/')
-def index():
-    return "<h1>Welcome to the GPT API</h1>"
-
-def generate_text(prompt):
-    completions = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    message = completions.choices[0].text.strip()
-    return message
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
+
--- Front-end/src/Components/mainpage.js
+++ Front-end/src/Components/mainpage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./mainpage.css";
+import "./mainpage.scss";
 
 function Mainpage() {
   const [inputHeight, setInputHeight] = useState("50px");
@@ -41,28 +42,30 @@ function Mainpage() {
       },
       body: JSON.stringify({ prompt }),
     });
-    console.log("here we are waiting, but not really getting a response!");
+    console.log("This is the response in the front-end!");
     const data = await response.json();
     console.log(data.message);
     // Update the UI with the generated text
     const generatedTextElement = document.createElement("p");
-    generatedTextElement.textContent = data.message;
     const contentElement = document.querySelector(".content");
     contentElement.appendChild(generatedTextElement);
+
+    // Slowly add each character to the element
+    for (let i = 0; i < data.message.length; i++) {
+      await new Promise((resolve) => setTimeout(resolve, 50)); // Delay each iteration by 50ms
+      generatedTextElement.textContent += data.message[i];
+    }
   };
 
   const text = "I'ma Predict You!";
-  const splitText = text
-    .split(" ")
-    .reduce((prev, current) => [...prev, current, "\u00A0"], []);
-
+  const splitText = text.split(" ").map((word, index) => (
+    <span className="predict" key={index}>
+      {word}&nbsp;
+    </span>
+  ));
   return (
     <div className="page">
-      <div className="header">
-        {splitText.map((char, index) => (
-          <span key={index}>{char}</span>
-        ))}
-      </div>
+      <div className="header">{splitText}</div>
       <div className="content">
         <p className="reader">
           Write me a scenario and I will predict what would happen next!
@@ -77,6 +80,17 @@ function Mainpage() {
         />
         <br />
       </div>
+
+      <a class="twitter" target="_top" href="https://twitter.com/omar13858982">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="72"
+          height="72"
+          viewBox="0 0 72 72"
+        >
+          <path d="M67.812 16.141a26.246 26.246 0 0 1-7.519 2.06 13.134 13.134 0 0 0 5.756-7.244 26.127 26.127 0 0 1-8.313 3.176A13.075 13.075 0 0 0 48.182 10c-7.229 0-13.092 5.861-13.092 13.093 0 1.026.118 2.021.338 2.981-10.885-.548-20.528-5.757-26.987-13.679a13.048 13.048 0 0 0-1.771 6.581c0 4.542 2.312 8.551 5.824 10.898a13.048 13.048 0 0 1-5.93-1.638c-.002.055-.002.11-.002.162 0 6.345 4.513 11.638 10.504 12.84a13.177 13.177 0 0 1-3.449.457c-.846 0-1.667-.078-2.465-.231 1.667 5.2 6.499 8.986 12.23 9.09a26.276 26.276 0 0 1-16.26 5.606A26.21 26.21 0 0 1 4 55.976a37.036 37.036 0 0 0 20.067 5.882c24.083 0 37.251-19.949 37.251-37.249 0-.566-.014-1.134-.039-1.694a26.597 26.597 0 0 0 6.533-6.774z"></path>
+        </svg>
+      </a>
     </div>
   );
 }
